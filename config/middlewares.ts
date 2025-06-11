b60cf477a5e34f006f3bede65c03504a38afd30f
brianGelvez <briannn97@gmail.com>
2025-06-11 08:51:41 -0300

    feat: Configurando correctamente clodinary servidor de imagenes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,16 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "img-src": ["'self'", "data:", "blob:", "https://market-assets.strapi.io", "https://res.cloudinary.com"],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
