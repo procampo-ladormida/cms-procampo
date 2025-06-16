482967a6d6cdd031b63b8a2fcef2c53f70d3ae19
brianGelvez <briannn97@gmail.com>
2025-06-16 04:02:25 -0300

    agregando relacion entre subcategorias y productos

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -522,6 +522,10 @@ export interface ApiProductoProducto extends Struct.CollectionTypeSchema {
       'specifications.specifications',
       true
     >;
+    subcategoria: Schema.Attribute.Relation<
+      'manyToOne',
+      'api::subcategoria.subcategoria'
+    >;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
@@ -555,6 +559,7 @@ export interface ApiSubcategoriaSubcategoria
     > &
       Schema.Attribute.Private;
     name: Schema.Attribute.String & Schema.Attribute.Required;
+    productos: Schema.Attribute.Relation<'oneToMany', 'api::producto.producto'>;
     publishedAt: Schema.Attribute.DateTime;
     slug: Schema.Attribute.UID<'name'>;
     updatedAt: Schema.Attribute.DateTime;
