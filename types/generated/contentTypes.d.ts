13acbaa099e11909d8a4eee75c35f644272e9f2b
Brian Gelvez <briannn97@gmail.com>
2025-06-22 16:43:48 -0300

    agregando etiqueta a producto en oferta

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -491,6 +491,7 @@ export interface ApiProductoEnOfertaProductoEnOferta
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    label: Schema.Attribute.String & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
